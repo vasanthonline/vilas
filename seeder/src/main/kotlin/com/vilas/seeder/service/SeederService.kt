ba044033f5b50eff4e39bf15df971ea15ced962a
Vasanth Gopal <vasanth.gopal0@loblaw.ca>
2023-09-23 19:03:27 -0400

    seed urls - save to firestore as well as publish to topic

--- seeder/src/main/kotlin/com/vilas/seeder/service/SeederService.kt
+++ seeder/src/main/kotlin/com/vilas/seeder/service/SeederService.kt
@@ -5,9 +5,11 @@ import com.vilas.seeder.firestore.repository.SeederRepository
 import com.vilas.seeder.model.SeederMessage
 import com.vilas.seeder.pubsub.SeederPublisher
 import com.vilas.seeder.web.models.SeederRequest
+import kotlinx.coroutines.reactive.collect
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import reactor.kotlin.core.publisher.toFlux
 import java.util.*
 
 @Service
@@ -16,30 +18,37 @@ class SeederService(
     private val seederPublisher: SeederPublisher,
 ) {
     fun seedUrls(seederRequest: SeederRequest): Flux<SeederDocument> {
-        val seederDocuments = toSeederDocuments(seederRequest)
+        val (jobId, seederDocuments) = toSeederDocuments(seederRequest)
 
         return seederRepository.saveAll(seederDocuments)
             .doOnSubscribe {
-                logger.info("Seeded ${seederRequest.urls.size} urls successfully with job Id ${seederDocuments[0].jobId}.")
+                logger.info("Seeded ${seederRequest.urls.size} urls successfully with job Id ${jobId}.")
             }
-            .map {
-                seederPublisher.publish(SeederMessage(
-                    documentId = it.id,
-                    url = it.url,
-                    jobId = it.jobId
-                ))
-                it
+            .collectMap { seederDocument ->
+                val seederMessage = SeederMessage(
+                    documentId = seederDocument.id,
+                    url = seederDocument.url,
+                    jobId = seederDocument.jobId
+                )
+                logger.info("Publishing seeder url ${seederDocument.url} with message: $seederMessage")
+                seederPublisher.publish(seederMessage)
+                .doOnSubscribe {
+                    logger.info("Published seeder url ${seederDocument.url} with message: $seederMessage")
+                }
+            }.flatMapIterable {
+                it.values
             }
     }
 
     companion object {
         private val logger = LoggerFactory.getLogger(SeederService::class.java)
 
-        private fun toSeederDocuments(seederRequest: SeederRequest): List<SeederDocument> {
-            val uuid = UUID.randomUUID().toString()
-            return seederRequest.urls.map {
-                SeederDocument(jobId = uuid, url = it)
-            }
+        private fun toSeederDocuments(seederRequest: SeederRequest): Pair<String, List<SeederDocument>> {
+            val jobId = UUID.randomUUID().toString()
+            return Pair(jobId, seederRequest.urls.map {
+                val documentId = UUID.randomUUID().toString()
+                SeederDocument(id = documentId, jobId = jobId, url = it)
+            })
         }
     }
 }
